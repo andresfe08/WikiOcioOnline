26a1e1b7c83b62651b250fe2fc69d85619a5c5a6
andresfe08 <96803035+andresfe08@users.noreply.github.com>
2024-02-10 19:58:22 -0500

    Nuevos archivos

---
+++ form.js
@@ -0,0 +1,4 @@
+// var nombre = prompt('Ingresa tu nombre: ');
+// var apellido = prompt('Ingresa tu apellido: ');
+// var url = "link" + nombre + "&entry.332941104="+ apellido;
+// window.location.href = url;
